89b74047dd66dc581a8f389a6d519d1fc5d7aee7
Zyadnoh2007 <161977609+Zyadnoh2007@users.noreply.github.com>
2025-11-19 02:13:27 +0200

    Create doctor.js

---
+++ questions/microbiology/doctor.js
@@ -0,0 +1,9 @@
+// لاحظ الاسم: window.microbiology_doctor_data
+window.microbiology_doctor_data = {
+  quiz1: {
+    title: "Doctor Quiz 1",
+    questions: [
+       { q: "New Doctor Question?", type: "mcq", options: ["Yes", "No"], a: 0, explanation: "Test" }
+    ]
+  }
+};
